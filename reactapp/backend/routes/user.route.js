472f249380b072400af9d2320a335aaf5e1bafcc
nguyendong98 <namasd2001@gmail.com>
2020-03-05 09:23:06 +0700

    delete

--- reactapp/backend/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router = express.Router()
-const userController = require('../controllers/user.controller')
-
-
-router.get('/', userController.index )
-router.post('/add', userController.add)
-
-module.exports = router
