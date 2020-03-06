8fac2552502a67ac1b8eaac21afe8450db8e9c0f
nhduy <nhduy220597@gmail.com>
2020-03-06 17:07:39 +0700

    fix warning

--- client/src/components/TopMenu/TopMenu.js
+++ client/src/components/TopMenu/TopMenu.js
@@ -7,17 +7,17 @@ export default function TopMenu() {
                 <div className="container">
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup text-center">
                         <div className="navbar-nav head__title-left">
-                            <a className="nav-item nav-link" href="#">Niên luận</a>
-                            <a className="nav-item nav-link" href="#" id="a-phone"><i className="fa fa-phone"
+                            <a className="nav-item nav-link" href="/">Niên luận</a>
+                            <a className="nav-item nav-link" href="/" id="a-phone"><i className="fa fa-phone"
                                     aria-hidden="true" />0783.777.999</a>
-                            <a className="nav-item nav-link" href="#" id="a-mail"><i className="fa fa-envelope-o"
+                            <a className="nav-item nav-link" href="/" id="a-mail"><i className="fa fa-envelope-o"
                                     aria-hidden="true" />nlnhom3@gmail.com</a>
                         </div>
                         <div className="navbar-nav text-center head__title-right">
-                            <a className="nav-item nav-link" href="#"><i className="fa fa-facebook" aria-hidden="true" /></a>
-                            <a className="nav-item nav-link" href="#"><i className="fa fa-twitter" aria-hidden="true" /></a>
-                            <a className="nav-item nav-link" href="#"><i className="fa fa-instagram" aria-hidden="true" /></a>
-                            <a className="nav-item nav-link" href="#"><i className="fa fa-youtube" aria-hidden="true" /></a>
+                            <a className="nav-item nav-link" href="facebook.com"><i className="fa fa-facebook" aria-hidden="true"></i>.</a>
+                            <a className="nav-item nav-link" href="twitter.com"><i className="fa fa-twitter" aria-hidden="true"></i>.</a>
+                            <a className="nav-item nav-link" href="instagram.com"><i className="fa fa-instagram" aria-hidden="true"></i>.</a>
+                            <a className="nav-item nav-link" href="youtube.com"><i className="fa fa-youtube" aria-hidden="true"></i>.</a>
                         </div>
                     </div>
                 </div>
