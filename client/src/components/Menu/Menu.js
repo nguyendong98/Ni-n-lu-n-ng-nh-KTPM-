51795f3e3956d414ce3c3d619a125a099932e6cb
nhduy <nhduy220597@gmail.com>
2020-03-11 18:02:01 +0700

    fix

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,47 +1,100 @@
 import React, { Component } from 'react';
 import './Menu.scss';
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 class Menu extends Component {
-    render() {
-        return (
-            <section className="main-menu">
-                <div className="container">
-                    <nav className="navbar navbar-expand-lg navbar-light ">
-                    <Link to="/" exact="true"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlKoNFjSRZhti9Mo2F-jeMuYbwp-qROryOF_5T0H242IOloTa6" style={{width: '7rem', height: '7rem'}} alt="logo"/></Link>
-                        
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <div className="navbar-nav" style={{margin: '0 auto'}}>
-                                <Link to="/" exact="true" className="nav-item nav-link " href="/"><i className="fa fa-home" aria-hidden="true" />Home</Link>
-                                <a className="nav-item nav-link " href="/"><i className="fa fa-bed" aria-hidden="true" />Room</a>
-                                <li className="dropdown " id="navbarNavDropdown">
-                                    <a className="nav-item nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i className="fa fa-glass" aria-hidden="true" />
-                                        Service
-                                    </a>
-                                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                        <Link to="/Tours" exact="true" className="dropdown-item" href="/">Tours info</Link>
-                                        <Link to="/Spa" className="dropdown-item" href="/">Spa</Link>
-                                        <Link to ="Restaurant" className="dropdown-item" href="/">Restaurant</Link>
-                                    </div>
-                                </li>
-                                <a className="nav-item nav-link " href="/"><i className="fa fa-camera-retro"
-                                        aria-hidden="true" />Gallery</a>
-                                <a className="nav-item nav-link " href="/"><i className="fa fa-gift"
-                                        aria-hidden="true" />Promotion</a>
-                                <a className="nav-item nav-link " href="/"><i className="fa fa-bell" aria-hidden="true" />Book
-                                    Now</a>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-            </section>
-        );
-    }
-}
+  render() {
+    return (
+      <section className='main-menu'>
+        <div className='container'>
+          <nav className='navbar navbar-expand-lg navbar-light '>
+            <Link to='/' exact='true'>
+              <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlKoNFjSRZhti9Mo2F-jeMuYbwp-qROryOF_5T0H242IOloTa6'
+                style={{ width: '7rem', height: '7rem' }}
+                alt='logo'
+              />
+            </Link>
 
+            <button
+              className='navbar-toggler'
+              type='button'
+              data-toggle='collapse'
+              data-target='#navbarSupportedContent'
+              aria-controls='navbarSupportedContent'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+            >
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            <div
+              className='collapse navbar-collapse'
+              id='navbarSupportedContent'
+            >
+              <div className='navbar-nav' style={{ margin: '0 auto' }}>
+                <Link
+                  to='/'
+                  exact='true'
+                  className='nav-item nav-link '
+                  href='/'
+                >
+                  <i className='fa fa-home' aria-hidden='true' />
+                  Home
+                </Link>
+                <a className='nav-item nav-link ' href='/'>
+                  <i className='fa fa-bed' aria-hidden='true' />
+                  Room
+                </a>
+                <li className='dropdown ' id='navbarNavDropdown'>
+                  <a
+                    className='nav-item nav-link dropdown-toggle'
+                    href='/'
+                    id='navbarDropdownMenuLink'
+                    data-toggle='dropdown'
+                    aria-haspopup='true'
+                    aria-expanded='false'
+                  >
+                    <i className='fa fa-glass' aria-hidden='true' />
+                    Service
+                  </a>
+                  <div
+                    className='dropdown-menu'
+                    aria-labelledby='navbarDropdownMenuLink'
+                  >
+                    <Link
+                      to='/Tours'
+                      exact='true'
+                      className='dropdown-item'
+                      href='/'
+                    >
+                      Tours info
+                    </Link>
+                    <Link to='/Spa' className='dropdown-item' href='/'>
+                      Spa
+                    </Link>
+                    <Link to='Restaurant' className='dropdown-item' href='/'>
+                      Restaurant
+                    </Link>
+                  </div>
+                </li>
+                <a className='nav-item nav-link ' href='/'>
+                  <i className='fa fa-camera-retro' aria-hidden='true' />
+                  Gallery
+                </a>
+                <a className='nav-item nav-link ' href='/'>
+                  <i className='fa fa-gift' aria-hidden='true' />
+                  Promotion
+                </a>
+                <a className='nav-item nav-link ' href='/'>
+                  <i className='fa fa-bell' aria-hidden='true' />
+                  Book Now
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Menu;
--- client/src/components/TopMenu/TopMenu.js
+++ client/src/components/TopMenu/TopMenu.js
@@ -1,27 +1,49 @@
-import React from 'react'
-import './TopMenu.scss'
+import React from 'react';
+import './TopMenu.scss';
 export default function TopMenu() {
-    return (
-        <section className="header">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light head__title">
-                <div className="container">
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup text-center">
-                        <div className="navbar-nav head__title-left">
-                            <a className="nav-item nav-link" href="/">Niên luận</a>
-                            <a className="nav-item nav-link" href="/" id="a-phone"><i className="fa fa-phone"
-                                    aria-hidden="true" />0783.777.999</a>
-                            <a className="nav-item nav-link" href="/" id="a-mail"><i className="fa fa-envelope-o"
-                                    aria-hidden="true" />nlnhom3@gmail.com</a>
-                        </div>
-                        <div className="navbar-nav text-center head__title-right">
-                            <a className="nav-item nav-link" href="facebook.com"><i className="fa fa-facebook" aria-hidden="true"></i>.</a>
-                            <a className="nav-item nav-link" href="twitter.com"><i className="fa fa-twitter" aria-hidden="true"></i>.</a>
-                            <a className="nav-item nav-link" href="instagram.com"><i className="fa fa-instagram" aria-hidden="true"></i>.</a>
-                            <a className="nav-item nav-link" href="youtube.com"><i className="fa fa-youtube" aria-hidden="true"></i>.</a>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </section>
-    )
+  return (
+    <section className='header'>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light head__title'>
+        <div className='container'>
+          <div
+            className='collapse navbar-collapse'
+            id='navbarNavAltMarkup text-center'
+          >
+            <div className='navbar-nav head__title-left'>
+              <a className='nav-item nav-link' href='/'>
+                Niên luận
+              </a>
+              <a className='nav-item nav-link' href='/' id='a-phone'>
+                <i className='fa fa-phone' aria-hidden='true' />
+                0783.777.999
+              </a>
+              <a className='nav-item nav-link' href='/' id='a-mail'>
+                <i className='fa fa-envelope-o' aria-hidden='true' />
+                nlnhom3@gmail.com
+              </a>
+            </div>
+            <div className='navbar-nav text-center head__title-right'>
+              <a className='nav-item nav-link' href='facebook.com'>
+                <i className='fa fa-facebook' aria-hidden='true'></i>.
+              </a>
+              <a className='nav-item nav-link' href='twitter.com'>
+                <i className='fa fa-twitter' aria-hidden='true'></i>.
+              </a>
+              <a className='nav-item nav-link' href='instagram.com'>
+                <i className='fa fa-instagram' aria-hidden='true'></i>.
+              </a>
+              <a className='nav-item nav-link' href='youtube.com'>
+                <i className='fa fa-youtube' aria-hidden='true'></i>.
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className='login'>
+        <a href='#' className='login-btn'>
+          Đăng nhập/đăng ký
+        </a>
+      </div>
+    </section>
+  );
 }
